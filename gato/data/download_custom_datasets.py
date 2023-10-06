ff68a198b1a1acdfda67f953503638eb7c7291aa
Daniel Lawson <daniellawson9999@gmail.com>
2023-10-05 22:13:45 -0400

    updated

--- gato/data/download_custom_datasets.py
+++ gato/data/download_custom_datasets.py
@@ -1,14 +1,20 @@
 import os
 import gdown
-
-datasets = {
+import argparse
+mujoco_datasets = {
     'd4rl_halfcheetah-expert-v2': 'https://drive.google.com/drive/folders/1YcUMTS7cMrUP8KJ6aQL87D9uYnrvGT02?usp=drive_link',
     'd4rl_hopper-expert-v2': 'https://drive.google.com/drive/folders/1upUt_aCRc3MCWhfVwpDlnW7YoVFEHre9?usp=drive_link',
     'd4rl_walker2d-expert-v2': 'https://drive.google.com/drive/folders/1ncu2DEhADWQBH6EeU_SrywQm8ETMM15M?usp=drive_link',
      #'Breakout-top1-s1-v0': 'https://drive.google.com/drive/folders/1Elos7A-NbpDzr5bPpPmoM-_2qY_68KFi?usp=drive_link' 
 }
 
+atari_top_1 = ''
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--download_group', type=str, default='MuJoCo', choices=['MuJoCo', 'TOP1_ATARI_TEST'])
+    args = parser.parse_args()
+
     minari_dir = os.path.join(os.path.expanduser('~'), '.minari')
     # create diretories if they do  not exist
     if not os.path.exists(minari_dir):
@@ -18,9 +24,14 @@ if __name__ == '__main__':
         os.mkdir(datasets_dir)
 
     # download datasets, if they do not exist already
-    for dataset_name, url in datasets.items():
-        target_path = os.path.join(datasets_dir, dataset_name)
-        if os.path.exists(target_path):
-            print(f'{dataset_name} already exists at {target_path}, skipping')
-            continue
-        gdown.download_folder(url=url, output=target_path, quiet=False, use_cookies=False)
+    if args.download_group == 'TOP1_ATARI_TEST':
+        gdown.download_folder(url=atari_top_1, output=datasets_dir, quiet=False, use_cookies=False)
+        #unzip the file
+        os.system(f'unzip {os.path.join(datasets_dir, "TOP1_ATARI_TEST.zip")} -d {datasets_dir}')
+    else:
+        for dataset_name, url in mujoco_datasets.items():
+            target_path = os.path.join(datasets_dir, dataset_name)
+            if os.path.exists(target_path):
+                print(f'{dataset_name} already exists at {target_path}, skipping')
+                continue
+            gdown.download_folder(url=url, output=target_path, quiet=False, use_cookies=False)
